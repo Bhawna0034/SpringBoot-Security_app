19ba58351731f76561fe377d9c74c55e86bf3101
Bhawna0034 <bhawnaasharma0034@gmail.com>
2024-10-08 11:49:22 +0530

    Jwt Token

--- src/test/java/com/bhawna/SpringBoot/Security_app/SecurityAppApplicationTests.java
+++ src/test/java/com/bhawna/SpringBoot/Security_app/SecurityAppApplicationTests.java
@@ -1,13 +1,26 @@
 package com.bhawna.SpringBoot.Security_app;
 
+import com.bhawna.SpringBoot.Security_app.entities.User;
+import com.bhawna.SpringBoot.Security_app.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityAppApplicationTests {
 
+	@Autowired
+	private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
+
+		User user = new User(4L, "bhawna@gmail.com", "1234");
+	    String token = jwtService.generateToken(user);
+		System.out.println(token);
+
+		Long userId = jwtService.getUserIdFromToken(token);
+		System.out.println(userId);
 	}
 
 }
